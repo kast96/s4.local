945947239b9f49b70c9c5eda9ca9e587b8d1fa87
kast96 <kast1996@gmail.com>
2021-08-12 18:59:24 +0300

    simple page

--- distance/index.php
+++ distance/index.php
@@ -2,8 +2,9 @@
 use Bitrix\Main\Loader;
 
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Дистанционное обучение');
 ?>
 
-Контакты
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa minus temporibus, maiores sequi voluptates alias porro magni corporis quibusdam vel soluta voluptate expedita quod accusantium, repellat est sint ratione iure.
 
 <?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php')?>
