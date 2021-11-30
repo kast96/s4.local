5d7a774936a757e21b920aeaa269c28f2af991d0
kast96 <kast96@yandex.ru>
2021-11-30 10:08:50 +0300

    contacts page

---
+++ contacts/index.php
@@ -0,0 +1,83 @@
+<?
+use Bitrix\Main\Loader;
+
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Контакты');
+
+$arPhones = CKastS4::GetPhonesArray();
+$email = CKastS4::GetEmail();
+$address = CKastS4::GetAddress();
+
+ $arPhonesFormatted = array_map(function($arPhone) {
+	 return '<a href="tel:'.$arPhone['TEL'].'">'.$arPhone['PHONE'].'</a>';
+}, $arPhones);
+?>
+
+<div class="contacts">
+	<div class="contacts-info">
+		<div class="contacts-item">
+			<div class="contacts-icon">
+				<i class="icon fa fa-phone"></i>
+			</div>
+			<div class="contacts-text">
+				<?=implode(', ', $arPhonesFormatted)?>
+				<?$APPLICATION->IncludeFile(
+					"/include/contacts-phones.php",
+					array(),
+					array("MODE"=>"html")
+				);?>
+			</div>
+		</div>
+		<div class="contacts-item">
+			<div class="contacts-icon">
+				<i class="icon fa fa-clock"></i>
+			</div>
+			<div class="contacts-text contacts-phone">
+				<?$APPLICATION->IncludeFile(
+					"/include/contacts-time.php",
+					array(),
+					array("MODE"=>"html")
+				);?>
+			</div>
+		</div>
+		<div class="contacts-item">
+			<div class="contacts-icon">
+				<i class="icon fa fa-envelope"></i>
+			</div>
+			<div class="contacts-text contacts-phone">
+				<a href="mailto:<?=$email?>"><?=$email?></a>
+				<?$APPLICATION->IncludeFile(
+					"/include/contacts-email.php",
+					array(),
+					array("MODE"=>"html")
+				);?>
+			</div>
+		</div>
+		<div class="contacts-item">
+			<div class="contacts-icon">
+				<i class="icon fa fa-map-marker-alt"></i>
+			</div>
+			<div class="contacts-text contacts-phone">
+				<span><?=$address?></span>
+				<?$APPLICATION->IncludeFile(
+					"/include/contacts-address.php",
+					array(),
+					array("MODE"=>"html")
+				);?>
+			</div>
+		</div>
+	</div>
+	<div class="contacts-map">
+		<div class="js-map" data-mapid="map-contacts" data-centercoord="[53.194769, 43.980995]">
+			<div class="js-map-item d-none" data-objectid="1" data-objectcoord="[53.194769, 43.980995]">
+				<span class="js-map-item-value">
+					<i class="contacts-points-icon las la-map-marker"></i>
+					МОУ СОШ №4<br>г.Каменка, ул.Чернышевского, 6
+				</span>
+			</div>
+			<div id="map-contacts" class="map"></div>
+		</div>
+	</div>
+</div>
+
+<?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php')?>
--- local/templates/s4/assets/js/bundle.js
+++ local/templates/s4/assets/js/bundle.js
@@ -5,12 +5,12 @@ GLOBAL.FORMERROR.REQUIRED = GLOBAL.FORMERROR.REQUIRED || '';
 GLOBAL.FORMERROR.EMAIL = GLOBAL.FORMERROR.EMAIL || '';
 
 GLOBAL.parseData = function parseData(data) {
-    try {
-        data = JSON.parse(data.replace(/'/gim, '"'));
-    } catch(e) {
-        data = {};
-    }
-    return data;
+	try {
+		data = JSON.parse(data.replace(/'/gim, '"'));
+	} catch(e) {
+		data = {};
+	}
+	return data;
 };
 
 GLOBAL.owl = GLOBAL.owl || {};
@@ -24,80 +24,80 @@ GLOBAL.owl.common.autoHeight = true;
 GLOBAL.owl.common.mouseDrag = true;
 GLOBAL.owl.common.navClass = ['owl-button owl-button-prev','owl-button owl-button-next'];
 GLOBAL.owl.common.navText = [
-    '<i class="owl-button-icon fa fa-chevron-left"></i>',
-    '<i class="owl-button-icon fa fa-chevron-right"></i>'
+	'<i class="owl-button-icon fa fa-chevron-left"></i>',
+	'<i class="owl-button-icon fa fa-chevron-right"></i>'
 ];
 
 
 
 function initPopupFeedback() {
-    $('.js-popup-feedback').each(function() {
-        $(this).fancybox({
-            type: 'ajax',
-            afterShow: function(instance, current) {
-                initForm();
-                initValidate();
-                initMask();
-            }
-        });
-    });
+	$('.js-popup-feedback').each(function() {
+		$(this).fancybox({
+			type: 'ajax',
+			afterShow: function(instance, current) {
+				initForm();
+				initValidate();
+				initMask();
+			}
+		});
+	});
 }
 
 function initForm() {
-    jQuery('.js-form').each(function() {
-        var $checkbox = $(this).find('.js-form-checkbox'),
-            $button = $(this).find('.js-form-button'),
-            classDisabled = $(this).data('form-disabled');
+	jQuery('.js-form').each(function() {
+		var $checkbox = $(this).find('.js-form-checkbox'),
+			$button = $(this).find('.js-form-button'),
+			classDisabled = $(this).data('form-disabled');
 
-        if ($checkbox.is(':checked')) {
-            $button.removeClass(classDisabled);
-        } else {
-            $button.addClass(classDisabled);
-        }
+		if ($checkbox.is(':checked')) {
+			$button.removeClass(classDisabled);
+		} else {
+			$button.addClass(classDisabled);
+		}
 
-        $checkbox.on("change", function(e) {
-            e.stopPropagation();
-            if ($checkbox.is(':checked')) {
-                $button.prop("disabled", false);
-                $button.removeClass(classDisabled);
-            } else {
-                $button.prop("disabled", true);
-                $button.addClass(classDisabled);
-            }
-        });
-    });
+		$checkbox.on("change", function(e) {
+			e.stopPropagation();
+			if ($checkbox.is(':checked')) {
+				$button.prop("disabled", false);
+				$button.removeClass(classDisabled);
+			} else {
+				$button.prop("disabled", true);
+				$button.addClass(classDisabled);
+			}
+		});
+	});
 }
 
 function initValidate($element) {
-    if (typeof($element) == 'undefined') {
-        $element = $('.js-form-validate');
-    }
+	if (typeof($element) == 'undefined') {
+		$element = $('.js-form-validate');
+	}
 
-    $element.each(function() {
-        var $element = jQuery(this),
-            validator;
+	$element.each(function() {
+		var $element = jQuery(this),
+			validator;
 
-        validator = $element.validate({
-            errorClass: 'form-error',
-            validClass: 'form-success',
-        });
+		validator = $element.validate({
+			errorClass: 'form-error',
+			validClass: 'form-success',
+		});
 
-        $.validator.messages.required = GLOBAL.FORMERROR.REQUIRED;
-        $.validator.messages.email = GLOBAL.FORMERROR.EMAIL;
-    });
+		$.validator.messages.required = GLOBAL.FORMERROR.REQUIRED;
+		$.validator.messages.email = GLOBAL.FORMERROR.EMAIL;
+	});
 }
 
 function initMask() {
-    $('.js-phone').mask('+7 (999) 999-99-99');
+	$('.js-phone').mask('+7 (999) 999-99-99');
 }
 
 $(document).on('submit', '.js-form-ajax', function() {
 	$this = $(this);
 
-    if ($this.hasClass('is-send'))
-        return false;
+	if ($this.hasClass('is-send'))
+		return false;
 
-    $this.addClass('is-send');
+	$this.addClass('is-send');
 	$.ajax({
 		url: $this.attr('action'),
 		type: "POST",
@@ -106,11 +106,11 @@ $(document).on('submit', '.js-form-ajax', function() {
 		success: function(result) {
 			$this.html($(result).find('.js-form-ajax').html());
 			initJsAfterAjaxForm();
-            $this.removeClass('is-send');
+			$this.removeClass('is-send');
 		},
 		error: function(response) {
 			$this.prepend('<div class="alert alert-danger">Request error</div>');
-            $this.removeClass('is-send');
+			$this.removeClass('is-send');
 		}
 	});
 	return false; 
@@ -153,173 +153,258 @@ $(document).on('click', '.js-pagen-ajax', function(e){
 });
 
 function initAdaptiveMenu() {
-    $('.js-adaptivemenu').each(function() {
-        var $navItemMore = $(this).find('.js-adaptivemenu-more'),
-            $navItems = $(this).find('.js-adaptivemenu-item'),
-            targetClass = '.js-adaptivemenu-target',
-            navItemWidth = $navItemMore.width(),
-            windowWidth = $(this).width();
+	$('.js-adaptivemenu').each(function() {
+		var $navItemMore = $(this).find('.js-adaptivemenu-more'),
+			$navItems = $(this).find('.js-adaptivemenu-item'),
+			targetClass = '.js-adaptivemenu-target',
+			navItemWidth = $navItemMore.width(),
+			windowWidth = $(this).width();
 
-        $navItemMore.before($(targetClass + ' > li'));
+		$navItemMore.before($(targetClass + ' > li'));
 
-        $navItems.each(function () {
-            navItemWidth += $(this).width();
-        });
+		$navItems.each(function () {
+			navItemWidth += $(this).width();
+		});
 
-        navItemWidth > windowWidth ? $navItemMore.show() : $navItemMore.hide();
+		navItemWidth > windowWidth ? $navItemMore.show() : $navItemMore.hide();
 
-        while (navItemWidth > windowWidth) {
-            navItemWidth -= $navItems.last().width();
-            $navItems.last().prependTo(targetClass);
-            $navItems.splice(-1, 1);
-        }
-    });
+		while (navItemWidth > windowWidth) {
+			navItemWidth -= $navItems.last().width();
+			$navItems.last().prependTo(targetClass);
+			$navItems.splice(-1, 1);
+		}
+	});
 }
 
 
 function initFix() {
-    if (typeof(Fix) === 'undefined' || !jQuery.isFunction(Fix)) {
-        return false;
-    }
+	if (typeof(Fix) === 'undefined' || !jQuery.isFunction(Fix)) {
+		return false;
+	}
 
-    var common = {
-        update: function (){
-            initAdaptiveMenu();
-        }
-    };
+	var common = {
+		update: function (){
+			initAdaptiveMenu();
+		}
+	};
 
-    $('.JS-Fix').not('.JS-Fix-ready').each(function(){
-        var local = GLOBAL.parseData(jQuery(this).data('fix'));
-        new Fix(this, jQuery.extend({}, common, local));
-    });
+	$('.JS-Fix').not('.JS-Fix-ready').each(function(){
+		var local = GLOBAL.parseData(jQuery(this).data('fix'));
+		new Fix(this, jQuery.extend({}, common, local));
+	});
 }
 
 function initMobileMenu() {
-    if (typeof(MobileMenu) === 'undefined' || !jQuery.isFunction(MobileMenu)) {
-        return false;
-    }
+	if (typeof(MobileMenu) === 'undefined' || !jQuery.isFunction(MobileMenu)) {
+		return false;
+	}
 
-    var common = {};
+	var common = {};
 
-    jQuery('.JS-MobileMenu').not('.JS-MobileMenu-ready').each(function() {
-        var local = GLOBAL.parseData(jQuery(this).data('mobilemenu'));
-        new MobileMenu(this, jQuery.extend({}, common, local));
-    });
+	jQuery('.JS-MobileMenu').not('.JS-MobileMenu-ready').each(function() {
+		var local = GLOBAL.parseData(jQuery(this).data('mobilemenu'));
+		new MobileMenu(this, jQuery.extend({}, common, local));
+	});
 }
 
 function initSliderBanner() {
-    $(".js-slider-banner").each(function(){
-        var $element = $(this),
-            $list = $element.find('.js-slider-list'),
-            $buttons = $element.find('.js-slider-buttons'),
-            $pager = $element.find('.js-slider-pager'),
-            $item = $list.find('.js-slider-item');
+	$(".js-slider-banner").each(function(){
+		var $element = $(this),
+			$list = $element.find('.js-slider-list'),
+			$buttons = $element.find('.js-slider-buttons'),
+			$pager = $element.find('.js-slider-pager'),
+			$item = $list.find('.js-slider-item');
 
-        if ($item.length > 1) {
-            $list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
-                nav: true,
-                autoHeight: false,
-                smartSpeed: 700,
-                responsive: {
-                    0: {
-                        items: 1,
-                    },
-                    720: {
-                        items: 1,
-                    },
-                    992: {
-                        items: 1,
-                    }
-                },
-                navContainer: $buttons,
-                dotsContainer: $pager,
-            }));
-        }
-    });
+		if ($item.length > 1) {
+			$list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
+				nav: true,
+				autoHeight: false,
+				smartSpeed: 700,
+				responsive: {
+					0: {
+						items: 1,
+					},
+					720: {
+						items: 1,
+					},
+					992: {
+						items: 1,
+					}
+				},
+				navContainer: $buttons,
+				dotsContainer: $pager,
+			}));
+		}
+	});
 }
 
 function initSlideParnters() {
-    $(".js-slider-partners").each(function(){
-        var $element = $(this),
-            $list = $element.find('.js-slider-list'),
-            $buttons = $element.find('.js-slider-buttons'),
-            $pager = $element.find('.js-slider-pager'),
-            $item = $list.find('.js-slider-item');
+	$(".js-slider-partners").each(function(){
+		var $element = $(this),
+			$list = $element.find('.js-slider-list'),
+			$buttons = $element.find('.js-slider-buttons'),
+			$pager = $element.find('.js-slider-pager'),
+			$item = $list.find('.js-slider-item');
 
-        if ($item.length > 1) {
-            $list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
-                nav: true,
-                autoHeight: false,
-                margin: 30,
-                smartSpeed: 500,
-                responsive: {
-                    0: {
-                        items: 1,
-                    },
-                    720: {
-                        items: 2,
+		if ($item.length > 1) {
+			$list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
+				nav: true,
+				autoHeight: false,
+				margin: 30,
+				smartSpeed: 500,
+				responsive: {
+					0: {
+						items: 1,
+					},
+					720: {
+						items: 2,
 
-                    },
-                    992: {
-                        items: 3,
-                    },
-                    1200: {
-                        items: 4,
-                    },
-                },
-                navContainer: $buttons,
-                dotsContainer: $pager,
-            }));
-        }
-    });
+					},
+					992: {
+						items: 3,
+					},
+					1200: {
+						items: 4,
+					},
+				},
+				navContainer: $buttons,
+				dotsContainer: $pager,
+			}));
+		}
+	});
 }
 
 function initSlideGallery() {
-    $(".js-slider-gallery").each(function(){
-        var $element = $(this),
-            $list = $element.find('.js-slider-list'),
-            $buttons = $element.find('.js-slider-buttons'),
-            $pager = $element.find('.js-slider-pager'),
-            $item = $list.find('.js-slider-item');
+	$(".js-slider-gallery").each(function(){
+		var $element = $(this),
+			$list = $element.find('.js-slider-list'),
+			$buttons = $element.find('.js-slider-buttons'),
+			$pager = $element.find('.js-slider-pager'),
+			$item = $list.find('.js-slider-item');
 
-        if ($item.length > 1) {
-            $list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
-                nav: true,
-                autoHeight: false,
-                margin: 30,
-                smartSpeed: 500,
-                responsive: {
-                    0: {
-                        items: 1,
-                    },
-                    720: {
-                        items: 2,
+		if ($item.length > 1) {
+			$list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
+				nav: true,
+				autoHeight: false,
+				margin: 30,
+				smartSpeed: 500,
+				responsive: {
+					0: {
+						items: 1,
+					},
+					720: {
+						items: 2,
 
-                    },
-                    992: {
-                        items: 3,
-                    },
-                },
-                navContainer: $buttons,
-                dotsContainer: $pager,
-            }));
-        }
-    });
+					},
+					992: {
+						items: 3,
+					},
+				},
+				navContainer: $buttons,
+				dotsContainer: $pager,
+			}));
+		}
+	});
 }
 
-$(document).ready(function () {
-    $(window).resize(function(){
-        initAdaptiveMenu();
-    });
 
-    initAdaptiveMenu();
-    initFix();
-    initValidate();
-    initForm();
-    initMask();
-    initMobileMenu();
-    initPopupFeedback();
-    initSliderBanner();
-    initSlideParnters();
-    initSlideGallery();
+ymaps.ready(init);
+
+function init () {
+	$('.js-map').each(function () {
+		var centerCoord = $(this).data('centercoord') || [59.93914514163674,30.33349282507063],
+			idMap = $(this).data('mapid'),
+			$item = $(this).find('.js-map-item'),
+			local = GLOBAL.parseData($(this).data('maplocal'));
+
+		var myMap = new ymaps.Map(idMap, jQuery.extend({}, local, {
+			center: centerCoord,
+			zoom: 16,
+		})),
+		objectManager = new ymaps.ObjectManager(),
+		masObjects =[];
+
+		//myMap.behaviors.disable('drag');
+		myMap.geoObjects.add(objectManager);
+
+		$item.each(function () {
+			var objectId = $(this).data('objectid'),
+			objectCoord = $(this).data('objectcoord'),
+			objectText =  $(this).find('.js-map-item-value').html();
+
+			var elementsObjects =
+			{
+				"type": "Feature",
+				"id": objectId,
+				"options": {
+					"preset": "islands#darkBlueIcon",
+				},
+				"geometry":{
+					"type": "Point",
+					"coordinates": objectCoord
+				},
+				"properties":{
+					"balloonContentBody": '<div class="map-popup">' +
+					'<div class="map-popup-body map-popup-body_simple">' + objectText + '</div>' +
+					'</div>',
+				}
+			};
+
+			masObjects.push(elementsObjects);
+		});
+
+		objectManager.add({
+			"type": "FeatureCollection",
+			"features": masObjects
+		});
+
+		objectManager.objects.events.add('click', function (e) {
+			var objectId=e.get('objectId');
+			viewObject(objectId);
+		});
+
+		[].forEach.call(document.querySelectorAll('[data-objectId]'), function(el) {
+			el.addEventListener('click', function() {
+			var objectId=el.getAttribute("data-objectId");
+			viewObject(objectId);
+			});
+		});
+
+		function viewObject(objectId){
+			$('.js-map-item').removeClass('active');
+
+			document.querySelector('[data-objectId="'+objectId+'"]').classList.add('active');
+
+			objectManager.objects.each(function (item) {
+				objectManager.objects.setObjectOptions(item.id, {
+				preset: 'islands#darkBlueIcon'
+			});
+			});
+			objectManager.objects.setObjectOptions(objectId, {
+			preset: 'islands#darkBlueIcon'
+			});
+
+			myMap.setCenter(objectManager.objects.getById(objectId).geometry.coordinates, 15, {
+			checkZoomRange: true
+			});
+		}
+	});
+}
+
+
+$(document).ready(function () {
+	$(window).resize(function(){
+		initAdaptiveMenu();
+	});
+
+	initAdaptiveMenu();
+	initFix();
+	initValidate();
+	initForm();
+	initMask();
+	initMobileMenu();
+	initPopupFeedback();
+	initSliderBanner();
+	initSlideParnters();
+	initSlideGallery();
 });
